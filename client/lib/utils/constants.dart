054fb010c6df32ca54600c6f33f2face8a256d85
anshRS <anshraj0426@gmail.com>
2023-06-21 18:09:13 +0530

    Created a utility file with constants for theme

---
+++ client/lib/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UtilityClass {
+  // Colors
+  static const primaryColor = Color(0xff0984e3);
+  static const primarybackgroundColor = Color(0xfff5f6fa);
+  static const Color secondarybackgroundColor = Color(0xffebecee);
+  static var selectedNavBarColor = Colors.cyan[800]!;
+  static const unselectedNavBarColor = Colors.black87;
+}
