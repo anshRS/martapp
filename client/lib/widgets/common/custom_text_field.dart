dcc308e3a0fe45f81186ec808f8e4d0fcf192180
anshRS <anshraj0426@gmail.com>
2023-06-21 18:10:21 +0530

    Created custom button & TextField widgets

---
+++ client/lib/widgets/common/custom_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final String labelText;
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.labelText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        label: Text(
+          labelText,
+          style: TextStyle(fontSize: 20),
+        ),
+        hintText: hintText,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black38,
+          ),
+        ),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.black38,
+        )),
+      ),
+      validator: (val) {},
+    );
+  }
+}
