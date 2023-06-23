61a0e0c93d0afda9496ea688e3c684a92afecb8b
anshRS <anshraj0426@gmail.com>
2023-06-24 01:27:43 +0530

    Created the endpoint for Sign Up

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from './routes/auth.js'
 
 // Configurations
 dotenv.config();
 const app = express();
 
+// Load Routes
+app.use(express.json());
+app.use('/auth', authRoutes);
+
 // Database Setup
 const PORT = process.env.PORT || '8000'
 mongoose.connect(process.env.MONGO_URL).then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
         console.log(`Server listening at ${PORT}`)
     })
 }).catch((err) => {
