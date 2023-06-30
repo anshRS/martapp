eac2f345f2e22193b314e330ba19f0ce7b068af6
anshRS <anshraj0426@gmail.com>
2023-06-30 19:41:28 +0530

    Created the endpoint for Sign In

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from '../controllers/auth.js'
+import { signup, signin } from '../controllers/auth.js'
 
 const router = express.Router();
 
 router.post("/api/signup", signup);
+router.post("/api/signin", signin);
 
 export default router;
