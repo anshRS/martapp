d913cdbb204abeea568b11d8ddf982b2ee0ffb6a
anshRS <anshraj0426@gmail.com>
2023-07-01 00:57:07 +0530

    Developed API to get user data

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { signup, signin } from '../controllers/auth.js'
+import { signup, signin, tokenIsValid, getUserData } from '../controllers/auth.js'
+import { verifyauth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/api/signup", signup);
 router.post("/api/signin", signin);
+router.get("/", verifyauth ,getUserData);
+router.post("/tokenIsValid", tokenIsValid);
 
 export default router;
